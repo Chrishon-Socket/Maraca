dce1f6cb9828e06eb6944dbe4cc1db80b9808c69
Chrishon <chrishon@socketmobile.com>
2019-11-25 15:51:37 -0500

    Replace single quotes/apostrophes with HTML/JS equivalent

--- Classes/Protocol+Declarations.swift
+++ Classes/Protocol+Declarations.swift
@@ -158,7 +158,7 @@ extension String {
                         "\n": "\\n",
                         "\r": "\\r",
                         "\"": "\\\"",
-                        "\'": "\\'",
+                        "'": "%27",
         ]
         
         return entities
